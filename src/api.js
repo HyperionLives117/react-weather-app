52c514cfa9449c784d4f43ecc26b194222ba2aa3
HyperionLives117 <makai.lambert@gmail.com>
2023-11-09 13:39:20 -0500

    finished project

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6738fb1f65msh37a536e607eaa40p10027ejsn5490d0452555',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+
+export const WEATHER_API_KEY = "replace later";
